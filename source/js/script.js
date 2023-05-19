ed9320394d98b573c274ab292a41b4122388a684
p1mk1n <evgen@p1mk1n.ru>
2023-05-19 19:30:52 +0300

    Добавляет фиксы страниц

--- source/js/script.js
+++ source/js/script.js
@@ -4,11 +4,14 @@ let navToggle = document.querySelector('.navigation__button');
 navMain.classList.remove('navigation--nojs');
 
 navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('navigation--closed')) {
-    navMain.classList.remove('navigation--closed');
-    navMain.classList.add('navigation--opened');
-  } else {
-    navMain.classList.add('navigation--closed');
-    navMain.classList.remove('navigation--opened');
+    navMain.classList.toggle('navigation--closed');
+    navMain.classList.toggle('navigation--opened');
   }
+);
+
+const icons = document.querySelectorAll('.burger');
+icons.forEach (icon => {
+  icon.addEventListener('click', (event) => {
+    icon.classList.toggle("open");
+  });
 });
