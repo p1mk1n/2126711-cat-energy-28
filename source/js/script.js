c39d5737370003e92394c716a08733a163950a59
p1mk1n <evgen@p1mk1n.ru>
2023-06-08 17:52:51 +0300

    Добавляет nojs для раздела с картой

--- source/js/script.js
+++ source/js/script.js
@@ -2,8 +2,10 @@ const navMain = document.querySelector('.navigation');
 const navToggle = document.querySelector('.header__button');
 const header = document.querySelector('.header')
 const icon = document.querySelector('.burger');
+const map = document.querySelector('.map');
 
 header.classList.remove('header--nojs');
+map.classList.remove('map--nojs');
 
 navToggle.addEventListener('click', function () {
     navMain.classList.toggle('navigation--closed');
